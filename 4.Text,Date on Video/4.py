d9142fe728b27305bbcfb4157c460315670ea8cf
shxrky17 <144344740+shxrky17@users.noreply.github.com>
2024-09-11 10:50:25 +0530

    Name organize

--- 4.Text,Date on Video/4.py
+++ 4.Text,Date on Video/4.py
@@ -10,9 +10,9 @@ while(True):
     ret,frame=cap.read()
     if ret==True:
         font=cv2.FONT_HERSHEY_SIMPLEX
-        text='Width:'+str(cap.get(cv2.CAP_PROP_FRAME_WIDTH))+'Height:'+str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        text='aksht is bot'
         datet=str(datetime.datetime.now())
-        frame=cv2.putText(frame,datet,(10,50),font,1,(0,255,255),2,cv2.LINE_AA)
+        frame=cv2.putText(frame,datet,(10,50),font,1,(0,0,0),2,cv2.LINE_AA)
         
     
         cv2.imshow('frame',frame)
